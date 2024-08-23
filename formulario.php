5561669b76321b4fe6e94f15341c1819b310af52
facudam <facundo89caceres@gmail.com>
2024-08-22 23:06:39 -0300

    Send Multiple select and checkbox values with POST method

--- formulario.php
+++ formulario.php
@@ -13,16 +13,24 @@
         </div>
         <br>
         <label for="asignatura">Asignatura</label>
-        <select id="asignatura" name="asignatura">
+        <select id="asignatura" name="asignatura[]" multiple>
             <option value="Ingles">Ingles</option>
             <option value="Matematica">Matemática</option>
             <option value="Ciencia">Ciencia</option>
             <option value="Lenguaje">Lenguaje</option>
         </select>
         <br><br>
-        <label for="frutas">
-            <input type="checkbox" value="Manzana" id="opcion-1" name="frutas">
+        <label for="opcion-1">
+            <input type="checkbox" value="Manzana" id="opcion-1" name="frutas[]">
             Manzana
+        </label>
+        <label for="opcion-2">
+            <input type="checkbox" value="Naranja" id="opcion-2" name="frutas[]">
+            Naranja
+        </label>
+        <label for="opcion-3">
+            <input type="checkbox" value="Pera" id="opcion-3" name="frutas[]">
+            Pera
         </label>
         <br><br><br>
         <button type="submit">Enviar</button>
--- index.php
+++ index.php
@@ -1,3 +1,13 @@
+<?php 
+$asignaturas = $_POST['asignatura'];
+$frutas = $_POST['frutas'];
+foreach($asignaturas as $materia) {
+    echo $materia . "<br>";
+}
+foreach($frutas as $fruta) {
+    echo $fruta . "<br>";
+}
+?>
 <h1><?= $_POST['nombre']  ?></h1> <!--Asi obtenemos los valores -->
-<h1><?= $_POST['asignatura']  ?></h1>
-<h1><?= $_POST['frutas']  ?></h1>
+
+
